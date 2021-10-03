b672521544a2b154986f6d1b11c0d79a3b6a27b4
Brant Shapka <bshapka@gmail.com>
2021-10-03 16:29:54 -0700

    added ResultTest class with tests

--- src/test/model/ResultTest.java
+++ src/test/model/ResultTest.java
@@ -1,4 +1,55 @@
 package model;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ResultTest {
+
+    Result result;
+    static final int DEFAULT_DIFFICULTY = (Result.MAX_DIFFICULTY + Result.MIN_DIFFICULTY) / 2;
+
+    @BeforeEach
+    void setup() {
+        result = new Result(DEFAULT_DIFFICULTY);
+    }
+
+    @Test
+    void testGetDifficulty() {
+        assertEquals(DEFAULT_DIFFICULTY, result.getDifficulty());
+    }
+
+    @Test
+    void testGetMinDifficulty() {
+        assertEquals(Result.MIN_DIFFICULTY, result.getMinDifficulty());
+    }
+
+    @Test
+    void testGetMaxDifficulty() {
+        assertEquals(Result.MAX_DIFFICULTY, result.getMaxDifficulty());
+    }
+
+    @Test
+    void testGetDateTime() {
+        assertEquals(LocalDateTime.now().toLocalDate(), result.getDateTime().toLocalDate());
+        assertEquals(LocalDateTime.now().toLocalTime().getHour(), result.getDateTime().toLocalTime().getHour());
+        assertEquals(LocalDateTime.now().toLocalTime().getMinute(), result.getDateTime().toLocalTime().getMinute());
+        assertEquals(LocalDateTime.now().toLocalTime().getSecond(), result.getDateTime().toLocalTime().getSecond(), 10);
+    }
+
+    @Test
+    void testWasDifficultTrue() {
+        result = new Result(Result.MAX_DIFFICULTY);
+        assertTrue(result.wasDifficult());
+    }
+
+    @Test
+    void testWasDifficultFalse() {
+        result = new Result(Result.MIN_DIFFICULTY);
+        assertFalse(result.wasDifficult());
+    }
+
 }
