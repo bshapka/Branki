71771c808f8d50537cffddd4f138515d459003fc
Brant Shapka <bshapka@gmail.com>
2021-10-03 20:31:38 -0700

    added BrankiApp ui class, added call to BrankiApp constructor in Main

---
+++ src/main/ui/BrankiApp.java
@@ -0,0 +1,17 @@
+package ui;
+
+import model.Deck;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BrankiApp {
+
+    private List<Deck> decks;
+
+    public BrankiApp() {
+        decks = new ArrayList<>();
+        System.out.println("Hello world");
+    }
+
+}
