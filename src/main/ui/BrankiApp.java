626207f81a9486fecc647699dfa00a86920aaa17
Brant Shapka <bshapka@gmail.com>
2021-10-04 18:07:17 -0700

    added class and method comments

--- src/main/ui/BrankiApp.java
+++ src/main/ui/BrankiApp.java
@@ -5,13 +5,21 @@ import model.Deck;
 import java.util.ArrayList;
 import java.util.List;
 
+// represents the Branki application console UI
 public class BrankiApp {
 
     private List<Deck> decks;
 
+    // MODIFIES: this
+    // EFFECTS: initializes decks to empty list and starts UI
     public BrankiApp() {
         decks = new ArrayList<>();
-        System.out.println("Hello world");
+        printWelcomeMessage();
+    }
+
+    // EFFECTS: prints welcome message
+    private void printWelcomeMessage() {
+        System.out.println("Welcome!");
     }
 
 }
--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,6 +1,9 @@
 package ui;
 
+// main entry point for application
 public class Main {
+
+    // EFFECTS: starts application
     public static void main(String[] args) {
         new BrankiApp();
     }
