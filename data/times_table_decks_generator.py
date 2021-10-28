eeba5bbab9f6b26521c39de50a5c1ad31e366b67
Brant Shapka <bshapka@gmail.com>
2021-10-28 14:14:19 -0700

    modified Python script to randomize order of cards and updated decks with output

--- data/times_table_decks_generator.py
+++ data/times_table_decks_generator.py
@@ -1,3 +1,5 @@
+import random
+
 decks = []
 deck = {"name": "Times Tables", "cards": []}
 decks.append(deck)
@@ -5,7 +7,8 @@ decks.append(deck)
 for i in range(11):
     for j in range(11):
         card = {"question": f"What is {i} times {j}?",
-                "answer": f"{i} * {j}", "results": []}
+                "answer": f"{i * j}", "results": []}
         decks[0]["cards"].append(card)
 
+random.shuffle(decks[0]['cards'])
 print(decks)
