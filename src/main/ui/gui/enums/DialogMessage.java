5d05539d28758f25ac3f1575ba8285c155f593e3
Brant Shapka <bshapka@gmail.com>
2021-11-07 22:31:57 -0800

    added DECK_DELETED, DECK_UPDATED, and NO_DECKS

--- src/main/ui/gui/enums/DialogMessage.java
+++ src/main/ui/gui/enums/DialogMessage.java
@@ -10,7 +10,13 @@ public enum DialogMessage {
         "You don't have any decks with cards.",
         "Please ensure there is a deck with a card and retry.")),
     STUDY_DECK_HAS_NO_CARDS("The deck to study has no cards."),
-    STUDY_SESSION_COMPLETE("Study session has been completed!");
+    STUDY_SESSION_COMPLETE("Study session has been completed!"),
+    DECK_DELETED("The deck has been deleted."),
+    DECK_UPDATED("The deck has been updated."),
+    NO_DECKS(MessageFormat.format(
+            "<html>{0}<br>{1}</html>",
+            "You don't have any decks.",
+            "Please ensure there is a deck and retry."));
 
     private final String message;
 
