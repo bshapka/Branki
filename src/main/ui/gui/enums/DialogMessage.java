fcf9b709a14876e74acb3c1dca710e08d656cb79
Brant Shapka <bshapka@gmail.com>
2021-11-08 19:44:06 -0800

    changed wording of some messages

--- src/main/ui/gui/enums/DialogMessage.java
+++ src/main/ui/gui/enums/DialogMessage.java
@@ -7,7 +7,7 @@ public enum DialogMessage {
 
     NO_DECKS_WITH_CARDS(MessageFormat.format(
         "<html>{0}<br>{1}</html>",
-        "You don't have any decks with cards.",
+        "There aren't any decks with cards.",
         "Please ensure there is a deck with a card and retry.")),
     STUDY_DECK_HAS_NO_CARDS("The deck to study has no cards."),
     STUDY_SESSION_COMPLETE("Study session has been completed!"),
@@ -15,7 +15,7 @@ public enum DialogMessage {
     DECK_UPDATED("The deck has been updated."),
     NO_DECKS(MessageFormat.format(
             "<html>{0}<br>{1}</html>",
-            "You don't have any decks.",
+            "There aren't any decks.",
             "Please ensure there is a deck and retry.")),
     DECK_CREATED("The deck has been created."),
     CARD_CREATED("The card has been created."),
