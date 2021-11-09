e80d583f8f8da4bb9905be6a10d5b40358c111c9
Brant Shapka <bshapka@gmail.com>
2021-11-08 19:35:59 -0800

    added NoDecksWithCardsException and associated test class

--- src/main/exceptions/NoDecksException.java
+++ src/main/exceptions/NoDecksException.java
@@ -1,9 +1,9 @@
 package exceptions;
 
-// represents exception thrown in DeckSelector constructor if list of deck arg contains no decks with cards
+// represents exception thrown in StudyDeckSelector constructor if list of deck arg is empty
 public class NoDecksException extends Exception {
 
-    // EFFECTS: constructs an NoDecksWithCardsException using the argument as an error message
+    // EFFECTS: constructs an NoDecksException using the argument as an error message
     public NoDecksException(String message) {
         super(message);
     }
