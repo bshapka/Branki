c930337a9ed49b8587c94763fe74c816f4b8968f
Brant Shapka <bshapka@gmail.com>
2021-11-08 17:03:45 -0800

    added casting and mapping required to use Selectable

--- src/main/ui/gui/views/selectors/EditDeckSelector.java
+++ src/main/ui/gui/views/selectors/EditDeckSelector.java
@@ -2,18 +2,20 @@ package ui.gui.views.selectors;
 
 import exceptions.NoDecksException;
 import model.Deck;
+import model.Selectable;
 import ui.gui.GUI;
 import ui.gui.enums.DialogMessage;
 
 import javax.swing.*;
 import java.util.List;
+import java.util.stream.Collectors;
 
 // represents a deck selector used to select a deck for editing
-public class EditDeckSelector extends DeckSelector {
+public class EditDeckSelector extends Selector {
 
-    // EFFECTS: calls superclass constructor
+    // EFFECTS: calls superclass constructor, and then throws NoDecksException if decks is empty
     public EditDeckSelector(List<Deck> decks) throws NoDecksException {
-        super(decks);
+        super(decks.stream().map(d -> (Selectable) d).collect(Collectors.toList()), "Deck Selection");
         if (decks.isEmpty()) {
             throw new NoDecksException(DialogMessage.NO_DECKS.getMessage());
         }
@@ -23,7 +25,7 @@ public class EditDeckSelector extends DeckSelector {
     // EFFECTS: as described in superclass, but with specific actionListener that calls GUI.showEditDeckWindow
     protected void setupSubmitButton() {
         submitButton = new JButton("Select");
-        submitButton.addActionListener(e -> GUI.showEditDeckWindow(decks.get(list.getSelectedIndex())));
+        submitButton.addActionListener(e -> GUI.showEditDeckWindow((Deck) selectables.get(list.getSelectedIndex())));
     }
 
 }
--- src/main/ui/gui/views/selectors/StudyDeckSelector.java
+++ src/main/ui/gui/views/selectors/StudyDeckSelector.java
@@ -2,20 +2,22 @@ package ui.gui.views.selectors;
 
 import exceptions.NoDecksException;
 import model.Deck;
+import model.Selectable;
 import ui.gui.GUI;
 import ui.gui.enums.DialogMessage;
 
 import javax.swing.*;
 import java.util.List;
+import java.util.stream.Collectors;
 
 // represents a deck selector used to select a deck for a study session
-public class StudyDeckSelector extends DeckSelector {
+public class StudyDeckSelector extends Selector {
 
-    // EFFECTS: throws NoDecksException if decks is empty or contains a deck with no cards,
-    //          otherwise calls superclass constructor
+    // EFFECTS: calls superclass constructor, and then throws NoDecksException if decks is empty or
+    //          contains a deck with no cards
     public StudyDeckSelector(List<Deck> decks) throws NoDecksException {
-        super(decks);
-        if (this.decks.size() == 0 || !this.decks.stream().allMatch(Deck::hasCards)) {
+        super(decks.stream().map(d -> (Selectable) d).collect(Collectors.toList()), "Deck Selection");
+        if (decks.size() == 0 || !decks.stream().allMatch(Deck::hasCards)) {
             throw new NoDecksException(DialogMessage.NO_DECKS_WITH_CARDS.getMessage());
         }
     }
@@ -24,7 +26,7 @@ public class StudyDeckSelector extends DeckSelector {
     // EFFECTS: as described in superclass, but with specific actionListener that calls GUI.showStudyDeckWindow
     protected void setupSubmitButton() {
         submitButton = new JButton("Select");
-        submitButton.addActionListener(e -> GUI.showStudyDeckWindow(decks.get(list.getSelectedIndex())));
+        submitButton.addActionListener(e -> GUI.showStudyDeckWindow((Deck) selectables.get(list.getSelectedIndex())));
     }
 
 }
